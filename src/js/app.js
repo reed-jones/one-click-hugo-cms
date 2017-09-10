b46c3015ed71120af41f965bb53e4188556ed511
Reed Jones <reedjones@reedjones.com>
2017-09-10 08:30:42 -0600

    removed redirect code

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // JS Goes here - ES6 supported
-if (window.netlifyIdentity && !window.netlifyIdentity.currentUser()) {
-  window.netlifyIdentity.on('login', () => {
-    document.location.href = '/admin/';
-  });
-}
+// if (window.netlifyIdentity && !window.netlifyIdentity.currentUser()) {
+//   window.netlifyIdentity.on('login', () => {
+//     document.location.href = '/admin/';
+//   });
+// }
